2b0a6f2d1f55a6ed6d590c8d474b9c1b0ac0b72a
rozakcloud <rozakcloud@gmail.com>
2020-10-30 21:42:06 +0700

    fixing routes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/questionnaires/create', 'QuestionnaireController@create');
+Route::post('/questionnaires', 'QuestionnaireController@store');
+Route::get('/questionnaires/{questionnaire}', 'QuestionnaireController@show');
+
+Route::get('/questionnaires/{questionnaire}/questions/create', 'QuestionController@create');
+Route::post('/questionnaires/{questionnaire}/questions', 'QuestionController@store');
+Route::delete('/questionnaires/{questionnaire}/questions/{question}', 'QuestionController@destroy');
+
+Route::get('/surveys/{questionnaire}-{slug}', 'SurveyController@show');
+Route::post('/surveys/{questionnaire}-{slug}', 'SurveyController@store');
