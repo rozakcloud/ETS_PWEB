5fd4c62dc0aa28be68c1d305ebf0dc1946a41abb
rozakcloud <rozakcloud@gmail.com>
2020-10-30 15:03:53 +0700

    fix auth logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,14 +20,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/questionnaires/create', 'QuestionnaireController@create');
-Route::post('/questionnaires', 'QuestionnaireController@store');
-Route::get('/questionnaires/{questionnaire}', 'QuestionnaireController@show');
-
-Route::get('/questionnaires/{questionnaire}/questions/create', 'QuestionController@create');
-Route::post('/questionnaires/{questionnaire}/questions', 'QuestionController@store');
-Route::delete('/questionnaires/{questionnaire}/questions/{question}', 'QuestionController@destroy');
-
-Route::get('/surveys/{questionnaire}-{slug}', 'SurveyController@show');
-Route::post('/surveys/{questionnaire}-{slug}', 'SurveyController@store');
